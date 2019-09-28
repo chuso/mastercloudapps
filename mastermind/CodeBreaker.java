a9b77741afdf2a7dbd223a3d9edcf53a44318064
Jesús Ruiz-Ayúcar <chusete@gmail.com>
2019-09-28 22:10:10 +0200

    Create CodeBreaker

---
+++ mastermind/CodeBreaker.java
@@ -0,0 +1,52 @@
+package mastermind;
+
+public class CodeBreaker {
+
+    private static final int MAX_ATTEMPTS = 10;
+
+    private ProposedCombination[] attempts;
+    private int numberOfAttempts;
+
+    public void prepare() {
+        this.attempts = new ProposedCombination[CodeBreaker.MAX_ATTEMPTS];
+        numberOfAttempts = 0;
+    }
+
+    public void propose() {
+        attempts[numberOfAttempts] = new ProposedCombination();
+        attempts[numberOfAttempts].read();
+        numberOfAttempts++;
+    }
+
+    public ProposedCombination getLastProposedCombination() {
+        return attempts[numberOfAttempts-1];
+    }
+
+    public void writeAttempts() {
+        System.out.println(numberOfAttempts + " attempt(s):");
+    }
+
+    public void writeProposedCombinations() {
+        for (int i = 0; i < numberOfAttempts; i++) {
+            attempts[i].write();
+        }
+    }
+
+    public boolean isWinner() {
+        return getLastProposedCombination().isWinner();
+    }
+
+    public boolean isLoser() {
+        return numberOfAttempts == CodeBreaker.MAX_ATTEMPTS;
+    }
+
+    public boolean isFinished() {
+        String continueAnswer;
+        do {
+            System.out.print("Do you want to continue? (y/n): ");
+            continueAnswer = System.console().readLine();
+        } while (!continueAnswer.matches("[yn]"));
+        return continueAnswer.equals("n");
+    }
+
+}
