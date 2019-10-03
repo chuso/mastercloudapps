56b24b326a27f9193074302633e16a8223d19f18
Jesús Ruiz-Ayúcar <chusete@gmail.com>
2019-10-03 21:41:07 +0200

    Create a new project which represents second-week mastermind

---
+++ mastermindWeek2/Combination.java
@@ -0,0 +1,21 @@
+package mastermindWeek2;
+
+import java.util.List;
+import java.util.ArrayList;
+import mastermindWeek2.utils.WithConsoleModel;
+
+abstract class Combination extends WithConsoleModel {
+	
+	private static final int WIDTH = 4;
+
+	protected List<Color> colors;
+	
+	protected Combination (){
+		this.colors = new ArrayList<Color>();
+	}
+	
+	static int getWidth() {
+		return Combination.WIDTH;
+	}
+	
+}
---
+++ mastermindWeek2/Error.java
@@ -0,0 +1,20 @@
+package mastermindWeek2;
+
+import mastermindWeek2.utils.Console;
+
+enum Error {
+	DUPLICATED("Repeated colors"),
+	WRONG_CHARACTERS("Wrong colors, they must be: " + Color.allInitials()),
+	WRONG_LENGTH("Wrong proposed combination length");
+
+	private String message;
+
+	private Error(String message) {
+		this.message = message;
+	}
+
+	void writeln() {
+		new Console().writeln(this.message);		
+	}
+	
+}
