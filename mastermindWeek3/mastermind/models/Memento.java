42b58552b7d800afbd6e95b84cf7fc74ccd7e2d7
Jesús Ruiz-Ayúcar <chusete@gmail.com>
2019-10-24 20:08:06 +0200

    Create a mememto class for Game
    This class will be able to store the state of a Game.

---
+++ mastermindWeek3/mastermind/models/Memento.java
@@ -0,0 +1,54 @@
+package mastermindWeek3.mastermind.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Memento {
+
+    private SecretCombination secretCombination;
+
+    private List<ProposedCombination> proposedCombinations;
+
+    private List<Result> results;
+
+    private int attempts;
+
+    public Memento() {
+        this.proposedCombinations = new ArrayList<ProposedCombination>();
+		this.results = new ArrayList<Result>();
+    }
+
+    public void setSecretCombination(SecretCombination secretCombination) {
+        this.secretCombination = secretCombination;
+    }
+
+    public SecretCombination getSecretCombination() {
+        return secretCombination;
+    }
+
+    public void addProposedCombination(ProposedCombination proposedCombination) {
+        this.proposedCombinations.add(proposedCombination);
+    }
+
+    public ProposedCombination getProposedCombination(int index) {
+        return this.proposedCombinations.get(index);
+    }
+
+    public void addResult(Result result) {
+        this.results.add(result);
+    }
+
+    public Result getResult(int index) {
+        return this.results.get(index);
+    }
+
+    public void setAttempts(int attempts) {
+        this.attempts = attempts;
+    }
+
+    public int getAttempts() {
+        return this.attempts;
+    }
+
+
+}
