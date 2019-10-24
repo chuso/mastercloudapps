d57185a14dc13ab1f0c220f2848a6ae08d054bc8
Jesús Ruiz-Ayúcar <chusete@gmail.com>
2019-10-24 22:02:29 +0200

    Create base TCPIP class for mastermind

---
+++ mastermindWeek3/mastermind/distributed/TCPIP.java
@@ -0,0 +1,46 @@
+package mastermindWeek3.mastermind.distributed;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPIP extends mastermindWeek3.santaTecla.utils.TCPIP {
+
+    private static final int PORT = 2020;
+
+    public TCPIP(Socket socket) {
+        super(socket);
+    }
+
+    public TCPIP(ServerSocket serverSocket, Socket socket) {
+        super(serverSocket, socket);
+    }
+
+    public static TCPIP createClientSocket() {
+        try {
+            Socket socket = new Socket("localhost", TCPIP.PORT);
+            System.out.println("Cliente> Establecida conexion");
+            return new TCPIP(socket);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static TCPIP createServerSocket() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(TCPIP.PORT);
+            System.out.println("Servidor> Esperando conexion...");
+            Socket socket = serverSocket.accept();
+            System.out.println("Servidor> Recibida conexion de " + socket.getInetAddress().getHostAddress() + ":"
+                    + socket.getPort());
+            return new TCPIP(serverSocket, socket);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    // TODO: implement sender and receivers methods
+
+}
